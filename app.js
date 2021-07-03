6ace4c5da24c7b2631dcb7eec5922ebc5095ace6
mrrwaa <51501630+mrrwaa@users.noreply.github.com>
2021-07-04 01:17:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.get("/useapp" , function(req,res){
 
 app.post("/useapp" , function(req,res){
   const textArea = req.body.text
-  const url = "https://f8aa21b8b6e8.ngrok.io/predict?text="+textArea 
+  const url = "https://f952cd82e36e.ngrok.io/predict?text="+textArea 
   
   https.request(url , function(res){
       res.on("data" , function(data){
